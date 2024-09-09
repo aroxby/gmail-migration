3f825dae6f212a734ffca70792e0a6575ec1deca
Andy Roxby <aroxby@users.noreply.github.com>
2024-09-09 11:27:30 -0400

    Transfer the messages!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import os.path
 from multiprocessing import Pool
+import os.path
+import sys
 from typing import Iterator
 
 from google.auth.transport.requests import Request
@@ -9,7 +10,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 
 class GMailClient:
@@ -17,7 +18,7 @@ class GMailClient:
         self._gmail_service = build("gmail", "v1", credentials=credentials)
 
     def list_labels(self) -> dict:
-        results = self._gmail_service.users().labels().list().execute()
+        results = self._gmail_service.users().labels().list(userId='me').execute()
         return results
 
     def get_label(self, label_id: str) -> dict:
@@ -49,6 +50,9 @@ class GMailClient:
             userId='me', id=message_id, format=message_format).execute()
         return message
 
+    def insert_message(self, message: dict) -> None:
+        self._gmail_service.users().messages().insert(userId='me', body=message).execute()
+
     def label_ids_by_name(self) -> dict[str, str]:
         labels = self.list_labels()
         return {label['name']: label['id'] for label in labels['labels']}
@@ -68,39 +72,65 @@ class GMailClient:
         return self.get_message(message['id'])
 
 
-def load_credentials() -> Credentials:
+def load_credentials(nickname) -> Credentials:
     credentials = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        credentials = Credentials.from_authorized_user_file("token.json", SCOPES)
+    token_file_name = f'token.{nickname}.json'
+    if os.path.exists(token_file_name):
+        credentials = Credentials.from_authorized_user_file(token_file_name, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not credentials or not credentials.valid:
         if credentials and credentials.expired and credentials.refresh_token:
             # TODO: Catch the error for expired refresh token
+            # maybe google.auth.exceptions.RefreshError
             credentials.refresh(Request())
         if not credentials or not credentials.valid:
             flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
+                    'credentials.json', SCOPES
             )
             credentials = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open(token_file_name, 'w') as token:
             token.write(credentials.to_json())
     return credentials
 
 
+def list_replace(lst: list, old, new) -> list:
+    output = [x if x != old else new for x in lst]
+    return output
+
+
+def drop_keys(data: dict, keys: list):
+    for key in keys:
+        data.pop(key, None)
+
+
 def main():
-    gmail = GMailClient(load_credentials())
-    expected_total = gmail.label_message_count('IMPORTANT')
-    processed = 0
-    raw_bytes_downloaded = 0
-    for message in gmail.for_each_message(label_ids=['IMPORTANT']):
-        if processed % 100 == 0:
-            print(f'{processed} / {expected_total}', raw_bytes_downloaded)
-        processed += 1
-        raw_bytes_downloaded += len(message.get('raw', ''))
+    if len(sys.argv) != 3:
+        print('usage: python3 main.py source-label destination-label')
+
+    print('Authenticating source account...')
+    src = GMailClient(load_credentials('src'))
+    print('Authenticating destination account...')
+    dst = GMailClient(load_credentials('dst'))
+
+    src_label_id = src.label_ids_by_name()[sys.argv[1]]
+    dst_label_id = dst.label_ids_by_name()[sys.argv[2]]
+
+    print('Processing messages...')
+    expected_total = src.label_message_count(src_label_id)
+    # TODO: To improve speed src.list_messages should be used here, then workers
+    # in a main pool can handle the download/modify/update step
+    for idx, message in enumerate(src.for_each_message(label_ids=[src_label_id])):
+        message['labelIds'] = list_replace(message['labelIds'], src_label_id, dst_label_id)
+        drop_keys(message, ['id', 'threadId', 'historyId'])
+        dst.insert_message(message)
+        if idx % 100 == 0:
+            print(f'{idx} / {expected_total}')
+
+    print('Done!')
 
 
 if __name__ == "__main__":
